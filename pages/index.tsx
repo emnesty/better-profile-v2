f05ed0d69555c198c910a9292224f6db53320136
Luciano Silva <legostr@gmail.com>
2023-04-04 14:23:26 -0300

    Sorts classes based on Tailwind guide

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,10 @@ export default function IndexPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="container col-span-2 items-center gap-9 pt-48 pb-9 pr-10 md:py-24">
-        <div className="flex col-span-2 gap-6 py-9 justify-between">
-          <div className="flex max-w-[625px] flex-col items-start gap-9 pb-9 py-9">
-            <h1 className="text-2xl max-w-[625px] font-extrabold leading-tight tracking-normal sm:text-3xl md:text-3xl lg:text-5xl">
+      <section className="container col-span-2 items-center gap-9 py-9 pr-10 pt-48 md:py-24">
+        <div className="col-span-2 flex justify-between gap-6 py-9">
+          <div className="flex max-w-[625px] flex-col items-start gap-9 py-9">
+            <h1 className="max-w-[625px] text-2xl font-extrabold leading-tight tracking-normal sm:text-3xl md:text-3xl lg:text-5xl">
               Seu LinkedIn pronto para o mercado de trabalho.{" "}
             </h1>
             <p className="max-w-[625px] text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
@@ -27,22 +27,22 @@ export default function IndexPage() {
               Sugerimos ações específicas para aumentar a visibilidade do seu
               perfil e atrair mais oportunidades profissionais.
             </p>
-            <div className="flex max-w-[625px flex-col items-start">
+            <div className="max-w-[625px flex flex-col items-start">
               <h1 className="flex max-w-full flex-col items-start pb-5">
                 Antes
               </h1>
-              <p className="text-lg text-slate-700 dark:text-slate-400 sm:text-sm px-8 pb-5 py-5 bg-white border border-gray-200 rounded-3xl dark:bg-gray-800 dark:border-gray-700">
+              <p className="rounded-3xl border border-gray-200 bg-white px-8 py-5 pb-5 text-lg text-slate-700 dark:border-gray-700 dark:bg-gray-800 dark:text-slate-400 sm:text-sm">
                 is simply dummy text of the printing and typesetting industry.
                 Lorem Ipsum has been the industry's standard dummy text ever
                 since the 1500s, when an unknown printer took a galley of type
                 and scrambled it to make a type specimen book.
               </p>
             </div>
-            <div className="flex max-w-[625px flex-col items-start">
+            <div className="max-w-[625px flex flex-col items-start">
               <h1 className="flex max-w-full flex-col items-start pb-5">
                 Depois
               </h1>
-              <p className="text-lg text-slate-700 dark:text-slate-400 sm:text-sm px-8 pb-5 py-5 bg-white border border-gray-200 rounded-3xl dark:bg-gray-800 dark:border-gray-700">
+              <p className="rounded-3xl border border-gray-200 bg-white px-8 py-5 pb-5 text-lg text-slate-700 dark:border-gray-700 dark:bg-gray-800 dark:text-slate-400 sm:text-sm">
                 is simply dummy text of the printing and typesetting industry.
                 Lorem Ipsum has been the industry's standard dummy text ever
                 since the 1500s, when an unknown printer took a galley of type
@@ -61,11 +61,11 @@ export default function IndexPage() {
               </h2>
             </div> */}
           </div>
-          <div className="flex max-w-[520px] justify-center flex-col items-end gap-9 px-8 bg-white border border-gray-200 rounded-3xl dark:bg-gray-800 dark:border-gray-700">
-            <h1 className="text-2xl w-full font-bold tracking-normal sm:text-3xl md:text-3xl lg:text-3xl">
+          <div className="flex max-w-[520px] flex-col items-end justify-center gap-9 rounded-3xl border border-gray-200 bg-white px-8 dark:border-gray-700 dark:bg-gray-800">
+            <h1 className="w-full text-2xl font-bold tracking-normal sm:text-3xl md:text-3xl lg:text-3xl">
               Better Profile{" "}
             </h1>
-            <h1 className="text-2xl w-full leading-tight tracking-normal sm:text-3xl md:text-3xl lg:text-2xl">
+            <h1 className="w-full text-2xl leading-tight tracking-normal sm:text-3xl md:text-3xl lg:text-2xl">
               Cadastre-se para o beta!{" "}
             </h1>
             <p className="max-w-[440px] text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
@@ -74,7 +74,7 @@ export default function IndexPage() {
             </p>
             <div className="grid w-full max-w-lg items-center gap-1.5">
               <Input type="email" id="email-2" placeholder="Email" />
-              <p className="text-sm text-slate-500 pb-5">
+              <p className="pb-5 text-sm text-slate-500">
                 Não se preocupe, não enviaremos nenhum spam.
               </p>
               <Button size="lg" type="submit">
