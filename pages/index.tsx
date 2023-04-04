cb6916fee6beee3def284fb4f97453ffc5ae73d3
Luciano Silva <legostr@gmail.com>
2023-04-04 14:01:54 -0300

    Novas seções

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from "next/head"
-import Link from "next/link"
-import { Label } from "@radix-ui/react-dropdown-menu"
 
-import { siteConfig } from "@/config/site"
 import { Layout } from "@/components/layout"
-import { Button, buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 export default function IndexPage() {
@@ -30,15 +27,26 @@ export default function IndexPage() {
               Sugerimos ações específicas para aumentar a visibilidade do seu
               perfil e atrair mais oportunidades profissionais.
             </p>
-            <div className="flex max-w-full flex-row items-start gap-9 pb-9 py-9">
-              <p className="text-lg text-slate-700 dark:text-slate-400 sm:text-sm pb-5 py-5 px-8">
-                + Visibilidade
+            <div className="flex max-w-[625px flex-col items-start">
+              <h1 className="flex max-w-full flex-col items-start pb-5">
+                Antes
+              </h1>
+              <p className="text-lg text-slate-700 dark:text-slate-400 sm:text-sm px-8 pb-5 py-5 bg-white border border-gray-200 rounded-3xl dark:bg-gray-800 dark:border-gray-700">
+                is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book.
               </p>
-              <p className="text-lg text-slate-700 dark:text-slate-400 sm:text-sm pb-5 py-5 px-8">
-                + Recomendações
-              </p>
-              <p className="text-lg text-slate-700 dark:text-slate-400 sm:text-sm pb-5 py-5 px-8">
-                + Engajamento
+            </div>
+            <div className="flex max-w-[625px flex-col items-start">
+              <h1 className="flex max-w-full flex-col items-start pb-5">
+                Depois
+              </h1>
+              <p className="text-lg text-slate-700 dark:text-slate-400 sm:text-sm px-8 pb-5 py-5 bg-white border border-gray-200 rounded-3xl dark:bg-gray-800 dark:border-gray-700">
+                is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book.
               </p>
             </div>
             {/* <div className="flex col-span-2 gap-20 py-9 justify-between">
@@ -54,17 +62,20 @@ export default function IndexPage() {
             </div> */}
           </div>
           <div className="flex max-w-[520px] justify-center flex-col items-end gap-9 px-8 bg-white border border-gray-200 rounded-3xl dark:bg-gray-800 dark:border-gray-700">
+            <h1 className="text-2xl w-full font-bold tracking-normal sm:text-3xl md:text-3xl lg:text-3xl">
+              Better Profile{" "}
+            </h1>
             <h1 className="text-2xl w-full leading-tight tracking-normal sm:text-3xl md:text-3xl lg:text-2xl">
-              Cadastre-se para o teste beta!{" "}
+              Cadastre-se para o beta!{" "}
             </h1>
             <p className="max-w-[440px] text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
-              Enviaremos convites para os que quiserem testar em primeira mão
-              nossa ferramenta.
+              Enviaremos em breve convites para os que quiserem testar nossa
+              ferramenta.
             </p>
             <div className="grid w-full max-w-lg items-center gap-1.5">
               <Input type="email" id="email-2" placeholder="Email" />
               <p className="text-sm text-slate-500 pb-5">
-                Não se preocupe, não enviaremos spam
+                Não se preocupe, não enviaremos nenhum spam.
               </p>
               <Button size="lg" type="submit">
                 Enviar
